ca0b694592795788661393fb9c566b40f8b8539b
NiranjanKumar003 <niranjankumarofficial003@gmail.com>
2024-11-09 04:06:08 +0530

    added heading sub-heading

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,20 @@
 import Image from "next/image";
+import SearchForm from "../components/SearchForm";
 
 export default function Home() {
   return (
     <>
-    <h1 className="heading">Home</h1>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch Your Startup, <br /> Connect With Entrepreneurs
+        </h1>
+
+        <p className="sub-heading !max-w-3xl">
+          Pitch your startup to a community of entrepreneurs, investors, and
+          industry experts.
+        </p>
+        <SearchForm />
+      </section>
     </>
-      );
+  );
 }
