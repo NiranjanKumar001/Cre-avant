c41e5eb9e076bcd5c5d5a767d27d48772aa39241
NiranjanKumar003 <niranjankumarofficial003@gmail.com>
2024-11-10 23:31:56 +0530

    chnaging the search button design

--- app/components/SearchForm.tsx
+++ app/components/SearchForm.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Form from "next/form";
 
 const SearchForm = () => {
   return (
-    <div>SearchForm</div>
+    <Form action="/" scroll={false} className='search-form'>SearchForm</Form>
+    <input
+    name="query"
+    defaultValue=""
+    className='search-input'
+    placeholder='Search Startups' 
+    />
   )
 }
 
