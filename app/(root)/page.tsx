07890faf5d331aff30a7dabd18555d426aad6014
NiranjanKumar003 <niranjankumarofficial003@gmail.com>
2024-11-11 20:49:44 +0530

    added card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,6 +4,20 @@ export default async function Home({searchParams}:{
   searchParams: Promise<{query?:string}>
 }) {
   const query =(await searchParams).query;
+
+  const posts =[
+    {
+      _createdAt:"Yesterday",
+      views:55,
+      author:{_id:1},
+      _id:1,
+      description:"This is a description",
+      image:"https://unsplash.com/photos/close-up-of-a-woman-hacker-hands-at-keyboard-computer-in-the-dark-room-at-night-cyberwar-concept-high-angle-view-YkibINt3MXo",
+      category:"Robots",
+      title:"We Robots",
+    },
+  ];
+
   return (
     <>
       <section className="pink_container">
@@ -22,6 +36,20 @@ export default async function Home({searchParams}:{
         <p className="text-30-semibold">
           {query?`Search results for "${query}"`:"All Startups"}
         </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length>0?(
+            posts.map(StartupCardType,number)=>(
+              <StartupCard/>
+            ))
+          ):(
+            <p className="no-results">
+              No startups found
+            </p>
+          )}
+
+        </ul>
+
       </section>
     </>
   );
