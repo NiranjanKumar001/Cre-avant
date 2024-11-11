c822a6f7ebebe274d733dfd9e6cb1d005ea7df97
NiranjanKumar003 <niranjankumarofficial003@gmail.com>
2024-11-12 03:38:02 +0530

    error fix

--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -1,10 +1,22 @@
+import { formatDate } from "@/lib/utils"
+import { EyeIcon } from "lucide-react"
+
 const StartupCard = ({post}:{StartupTypeCard}) => {
   return (
     <li className="startup-card group">
         <div className="flex-between">
             <p className="startup_card_date">
-                {post._createdAt}
+                {formatDate(post._createdAt)}
             </p>
+            <div className="flex gap-1.5">
+                <EyeIcon className="size-6 text-primary"/>
+                <span className="text-16-medium">{post.views}</span>
+            </div>
+        </div>
+        <div className="flex-between mt-5 gap-5">
+            <div >
+
+            </div>
         </div>
     </li>
   )
