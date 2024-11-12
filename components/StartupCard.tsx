cb32856e4b4c17c0f890478341ea3ab3cd548689
NiranjanKumar003 <niranjankumarofficial003@gmail.com>
2024-11-12 16:40:01 +0530

    fixed error

--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -3,8 +3,6 @@ import { EyeIcon } from "lucide-react";
 import Link from "next/link";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
-// import { Author, Startup } from "@/sanity/types";
-// import { Skeleton } from "@/components/ui/skeleton";
 
 export type StartupTypeCard = Omit<Startup, "author"> & { author?: Author };
 
@@ -67,15 +65,5 @@ const StartupCard = ({ post }: { post: StartupTypeCard }) => {
     </li>
   );
 };
-
-// export const StartupCardSkeleton = () => (
-//   <>
-//     {[0, 1, 2, 3, 4].map((index: number) => (
-//       <li key={cn("skeleton", index)}>
-//         <Skeleton className="startup-card_skeleton" />
-//       </li>
-//     ))}
-//   </>
-// );
 
 export default StartupCard;
