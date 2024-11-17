c6a3943a3278551fab9fd3dc96836bec9bb163a1
NiranjanKumar003 <niranjankumarofficial003@gmail.com>
2024-11-17 23:59:57 +0530

    skeleton added

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -1,11 +1,12 @@
+import React, { Suspense } from "react";
 import { formatDate } from "@/lib/utils";
 import { client } from "@/sanity/lib/client";
 import { STARTUP_BY_ID_QUERY } from "@/sanity/lib/queries";
 import Image from "next/image";
 import Link from "next/link";
 import { notFound } from "next/navigation";
-import React from "react";
 import markdownit from 'markdown-it';
+import { Skeleton } from "@/components/ui/skeleton";
 
 const md =markdownit();
 
@@ -68,7 +69,17 @@ const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
           )}
 
         </div>
+
+        <hr className="divider"/>
+
+        {/*EDITOR SELECTED startup */}
+
+        <Suspense fallback={<Skeleton/>}>
+
+        </Suspense>
       </section>
+
+
     </>
   );
 };
